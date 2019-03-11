624af4135f38eed27cc628a6d6d6c02bbb0a5cf6
rgruncharova <47794613+rgruncharova@users.noreply.github.com>
2019-03-12 00:21:59 +0200

    Add files via upload

---
+++ alcohol.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                double whiskeyPrice = double.Parse(Console.ReadLine());
+                double beer = double.Parse(Console.ReadLine());
+                double wine = double.Parse(Console.ReadLine());
+                double rakia = double.Parse(Console.ReadLine());
+                double whiskey = double.Parse(Console.ReadLine());
+                double rakiaLprice = whiskeyPrice / 2;
+                double wineLprice = rakiaLprice - (0.4 * rakiaLprice);
+                double beerLprice = rakiaLprice - (0.8 * rakiaLprice);
+                double rakiaprice = rakia * rakiaLprice;
+                double wineprice = wine * wineLprice;
+                double beerprice = beerLprice * beer;
+                double whiskeyprice = whiskey * whiskeyPrice;
+                double cost = rakiaprice + wineprice + beerprice + whiskeyprice;
+                Console.WriteLine("{0:F2}", cost);
+            }
+        }
+    }
+
+
---
+++ dancehall.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double hallLenght = double.Parse(Console.ReadLine());
+            double hallWidth = double.Parse(Console.ReadLine());
+            double wardrobeSide = double.Parse(Console.ReadLine());
+
+            double hallarea = (hallLenght * 100) * (hallWidth * 100);
+            double wardrobearea = wardrobeSide * 100 * wardrobeSide * 100;
+            double bench = hallarea / 10;
+            double free = hallarea - wardrobearea - bench;
+            double dancers = free / (40 + 7000);
+
+            Console.WriteLine(Math.Floor(dancers));
+
+        }
+    }
+}
